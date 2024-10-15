e709b683d054b8ad93541e6c6be19ca183407f79
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:11:38 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿
+public class Product
+{
+    //fields
+    public int productID;
+    public string productName;
+    public double cost;
+    public double tax;
+    public int quantityInStock;
+    public static int TotalNoProducts;
+    public const string CategoryName = "Electronics";
+    public readonly string dateOfPurchase;
+
+    //constructor
+    public Product()
+    {
+        dateOfPurchase = System.DateTime.Now.ToShortDateString();
+    }
+
+    //method
+    public void CalculateTax()
+    {
+        double t;
+        if (cost <= 20000)
+        {
+            t = cost * 10 / 100;
+        }
+        else
+        {
+            t = cost * 12.5 / 100;
+        }
+        tax = t;
+    }
+}
